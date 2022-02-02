cdda65cc0a7bbe12985c5d32d2ca9aa3dd975904
Carmen Maria <carmenxu290699@gmail.com>
2022-02-02 13:17:01 +0100

    resto del proyecto

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Proyecto_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            string mensajeDeSalida;
+
+
+        mensajeDeSalida = "Hello Mora";
+        Console.WriteLine(mensajeDeSalida);
+
+        mensajeDeSalida ="Estamos programando";
+    Console.WriteLine(mensajeDeSalida);
+        mensajeDeSalida = Console.ReadLine();
+     Console.WriteLine("Tu mensaje es" + mensajeDeSalida);
+    Console.ReadLine();
+
+        }
+
+
+
+    }
+}
